cb22be33b7b4e4a17ef4dc2ee1d77800cc422cd5
Caleb Xu <calebcenter@live.com>
2019-01-19 01:09:26 -0500

    Use new Blue Alliance v3 API
    Blue Alliance is soon deprecating the v2 API. Make the necessary
    changes in our script to use the v3 API instead. The new v3 API key
    requires an API key to be sent via headers. To generate an API key,
    log into your TBA account and access your account settings.

--- bluealliancefetch.sh
+++ bluealliancefetch.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 event="$1"
-curl --silent -H "X-TBA-App-Id: frc4828:insight:v1.0.0" http://www.thebluealliance.com/api/v2/event/$event/matches | jq '. | map(select(.alliances.blue.teams[0]=="frc4828", .alliances.blue.teams[1]=="frc4828", .alliances.blue.teams[2]=="frc4828", .alliances.red.teams[0]=="frc4828", .alliances.red.teams[1]=="frc4828", .alliances.red.teams[2]=="frc4828"))' | jq -s '. | sort_by("match_number")' | jq '.[0]' | jq 'sort_by(.time)'
+apikey="$2"
+info=$(curl --silent -H "X-TBA-App-Id: frc4828:insight:v1.0.0" -H "X-TBA-Auth-Key: $apikey" https://www.thebluealliance.com/api/v3/event/$event | jq 'with_entries(select(.key == "name" or .key == "key"))')
+matches=$(curl --silent -H "X-TBA-App-Id: frc4828:insight:v1.0.0" -H "X-TBA-Auth-Key: $apikey" https://www.thebluealliance.com/api/v2/event/$event/matches | jq '. | map(select(.alliances.blue.teams[0]=="frc4828", .alliances.blue.teams[1]=="frc4828", .alliances.blue.teams[2]=="frc4828", .alliances.red.teams[0]=="frc4828", .alliances.red.teams[1]=="frc4828", .alliances.red.teams[2]=="frc4828"))' | jq -s '. | sort_by("match_number")' | jq '.[0]' | jq 'sort_by(.time)')
+
+jq -n --argjson matches "$matches" --argjson info "$info" "$info + {matches: $matches}"
